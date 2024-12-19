6acce68d9d229890ad3c94e4d9d3659e5280e4f0
Rapolas <rapolaspreiksaitis@gmail.com>
2024-12-19 18:52:44 +0200

    wishes attempt 1

--- src/main/resources/public/sketch.js
+++ src/main/resources/public/sketch.js
@@ -1,6 +1,20 @@
+let wishes [] = [
+'palinkejimas1',
+'palinkejimas2',
+'palinkejimas3'
+]
 function setup(){
     createCanvas(windowWidth,windowHeight);
 }
 function draw(){
     background(0);
+    fill(255);
+    textAlign(CENTER,CENTER);
+    text(wishes[current_wish],width/2,height/2);
+    if(frameCount%(60*5)==0){
+        current_wish++;
+        if(current_wish>=wishes.length){
+            current_wish = 0;
+        }
+    }
 }
