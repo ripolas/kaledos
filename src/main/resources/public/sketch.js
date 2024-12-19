b3efa3f4314eb6fec4031b6ee09a0de6488d3139
Rapolas <rapolaspreiksaitis@gmail.com>
2024-12-19 19:31:47 +0200

    forgor 0,0 wasnt the center of the screen

--- src/main/resources/public/sketch.js
+++ src/main/resources/public/sketch.js
@@ -19,7 +19,7 @@ function draw(){
     textSize(min(width,height)/20);
     fill(0,100);
     rectMode(CENTER);
-    rect(0,0,min(width,height)/20,textWidth(wishes[current_wish]));
+    rect(width/2,height/2,min(width,height)/20,textWidth(wishes[current_wish]));
     fill(255);
     textAlign(CENTER,CENTER);
     text(wishes[current_wish],width/2,height/2);
