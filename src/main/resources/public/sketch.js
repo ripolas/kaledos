aa3462ffdde9c14b1e8197e70f28ed4dbf68fd59
Rapolas <rapolaspreiksaitis@gmail.com>
2024-12-19 19:52:04 +0200

    photo transitions?

--- src/main/resources/public/sketch.js
+++ src/main/resources/public/sketch.js
@@ -20,7 +20,7 @@ function draw(){
     background(0);
     if(frameCount%(60*4)<60&&current_photo!=0){
         noTint();
-        custom_image(photos[current_photo-1]);
+        custom_image(photos[0]);
         tint(map(frameCount%(60*4),0,60,0,255));
         custom_image(photos[current_photo]);
         noTint();
